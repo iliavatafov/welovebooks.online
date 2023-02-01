6ba00e08b4bcc1922fc6bab27fae5440c6fd5108
ilia vatafov <iliyavatafov@gmail.com>
2023-02-01 09:44:06 +0200

    Add fav icon and loading to Home Page

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,7 +1,4 @@
-import { useEffect } from "react";
-import { useContext } from "react";
-import { useState } from "react";
-
+import { useEffect, useContext, useState } from "react";
 import { LoadingContext } from "../../context/LoadingContext";
 
 import { GetAllOrders } from "../../apis/orders";
